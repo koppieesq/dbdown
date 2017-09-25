c0e330fed6c23ac07e638d6aaf7ec9e9340c1c71
Jordan <jordan@koplowicz.com>
2017-09-25 16:49:28 -0700

    adds support for Drupal 8

--- dbdown.sh
+++ dbdown.sh
@@ -3,6 +3,7 @@
 # First command line argument is the target site.  Second argument is the target environment.
 SITE=$1
 ENV=$2
+
 # Check for progress bar utilities (optional)
 CHECK1=$(which pv)
 CHECK2=$(which bar)
@@ -18,6 +19,14 @@ if [ -z "$2" ]; then
 fi
 NOW=$(date +"%Y%m%d")
 OUTPUT=$HOME/Downloads/$SITE.$ENV$NOW.sql.gz
+
+# Check Drupal 7 or Drupal 8
+VERSION=$(drush status | grep 'Drupal version')
+if [[ $VERSION == *":  8."* ]]; then
+  DCA='drush cr'
+else
+  DCA='drush cc all'
+fi
 
 echo "Hello!  Welcome to DB Down."
 echo "I will download the database from the \033[1;36m$ENV\033[0m environment of \033[1;36m$SITE\033[0m"
@@ -46,4 +55,4 @@ fi
 
 echo "All set!  Now a little housekeeping."
 drush updb -y
-drush cc all
+$DCA
